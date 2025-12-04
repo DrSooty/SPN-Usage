1200036060d8a015da146aacd71f270fdbcf1993
DrSooty <giraudocc@outlook.com>
2025-12-04 15:02:12 +1100

    New files for script
    working version of script - will eventually combine the files into one.

---
+++ NameFormat.ps1
@@ -0,0 +1,17 @@
+﻿#require file for script to run, use .\SPNScript -AccountListPasth .\filepath.txt to run
+param(
+[Parameter(Mandatory=$true)]
+[string]$AccountListPath
+)
+
+#imports txt file
+$AccountName = Get-Content -Path $AccountListPath | Where-Object { $_ -ne " " }
+
+
+foreach ($line in $AccountName) {
+$event = Get-WinEvent -LogName Security -FilterXPath "*[System/EventID=4769] and *[EventData/Data[@Name='ServiceName']='$line']" -MaxEvents 1 -ErrorAction SilentlyContinue
+if ( $event.Message -eq $null ){
+Write-host "No 4769 events for $line" -backgroundcolor "red" -foregroundcolor "white"
+} Else {
+Write-host "There are 4769 events for $line" -backgroundcolor "green" -foregroundcolor "black"
+}}
---
+++ showSPN.ps1
@@ -0,0 +1,19 @@
+﻿$MySearch = New-Object DirectoryServices.DirectorySearcher([ADSI]"")
+$MySearch.filter = "(&(objectClass=user)(objectCategory=user)(servicePrincipalName=*))"
+$MyResults = $MySearch.Findall()
+write-host $pwd
+
+foreach($result in $MyResults){
+ $userEntry = $result.GetDirectoryEntry()
+ $Service =  $userEntry.Name
+ write-host "Object Name = " $Service -backgroundcolor "yellow" -foregroundcolor "black"
+ Write-host "servicePrincipalNames"
+$i=1
+Add-Content -Path "./Service.txt" -Value $Service
+foreach($SPN in $userEntry.servicePrincipalName){
+  Write-host "SPN(" $i ")   = "   $SPN
+  $i+=1
+  }
+  Write-host ""
+}
+
